a4039347851b5fed4ac0cac7cfc47c3937b9436c
abhinav <jabhinav041@gmail.com>
2025-02-10 18:10:48 +0530

    fixed topbar

--- includes/headbar.php
+++ includes/headbar.php
@@ -1,4 +1,4 @@
-<div class="h-[10vh] bg-red-600 flex items-center justify-start px-8">
+<div class="fixed top-0 left-0 w-full h-[10vh] bg-red-600 flex items-center justify-start px-8 z-50">
     <a href="dashboard.php" class="text-white text-3xl font-bold hover:underline">
         Discord WebJam
     </a>
